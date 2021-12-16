8a49eebda40197eaea3c67ec619b05fed6017cb8
MaXiaomao <maxiaomaoemail@foxmail.com>
2021-12-16 08:09:38 +0800

    文章管理页面基本样式

--- src/pages/TheArticle/index.jsx
+++ src/pages/TheArticle/index.jsx
@@ -1,21 +1,33 @@
 import {useEffect, useRef, useState} from "react"
+import moment from "moment"
 import PubSub from "pubsub-js"
 import MarkdownIt from "markdown-it"
 import MdEditor from "react-markdown-editor-lite"
-import {Button, Drawer, Form, Input, Pagination, Select, Space, Table, DatePicker} from "antd"
+import {Button, Drawer, Form, Input, Pagination, Select, Space, Table, DatePicker, Row, Col} from "antd"
 import TitleBlock from "../../components/TitleBlock"
 import Screening from "../../components/Screening"
 import "react-markdown-editor-lite/lib/index.css"
 import "./index.css"
 
 const Index = function () {
+	const [screeningClassify, setScreeningClassify] = useState()
+	const [screeningTitle, setScreeningTitle] = useState()
 	const [dataSource, setDataSource] = useState([])
 	const [selectedRowKeys, setSelectedRowKeys] = useState([])
 	const [tableHeight, setTableHeight] = useState(0)
 	const [visible, setVisible] = useState(false)
+	const [formBody, setFormBody] = useState()
 	const tableBlock = useRef()
-	const [form] = Form.useForm()
+	const mdEditorRef = useRef()
+	const [formRef] = Form.useForm()
 
+	const screeningPass = () => {
+		console.log(screeningClassify, screeningTitle)
+	}
+	const screeningReturn = () => {
+		setScreeningClassify(null)
+		setScreeningTitle(null)
+	}
 	const onSelectChange = (selectId) => {
 		setSelectedRowKeys(selectId)
 	}
@@ -38,24 +50,25 @@ const Index = function () {
 			// )
 		}
 	}
-	const showDrawer = () => {
-		setVisible(true)
-	}
-	const onClose = () => {
+	const drawerFormReturn = () => {
+		formRef.resetFields()
 		setVisible(false)
 	}
-	const handleEditorChange = ({html, text}) => {
-		console.log("handleEditorChange", html, text)
-	}
-	const onClickForm = () => {
-		console.log(form.getFieldsValue())
+	const drawerFormPass = () => {
+		formRef.validateFields().then(
+			() => {
+				console.log(mdEditorRef.current.getMdValue())
+				console.log(formRef.getFieldsValue())
+				drawerFormReturn()
+			},
+			(err) => {
+				console.log(err)
+			}
+		)
 	}
 	const aaaaa = () => {
 		setVisible(true)
-		form.setFieldsValue({title: "从地上到处都是"})
-	}
-	const handleChange = (value) => {
-		console.log(`selected ${value}`)
+		formRef.setFieldsValue({title: "从地上到处都是"})
 	}
 
 	useEffect(() => {
@@ -173,21 +186,33 @@ const Index = function () {
 		<div className="the-article">
 			<Screening>
 				<div>
-					<Select placeholder="请选择文章分类" onChange={handleChange}>
+					<Select
+						value={screeningClassify}
+						onChange={(value) => setScreeningClassify(value)}
+						placeholder="请选择文章分类"
+					>
 						<Select.Option value="jack">Jack</Select.Option>
 						<Select.Option value="lucy">Lucy</Select.Option>
 						<Select.Option value="Yiminghe">yiminghe</Select.Option>
 					</Select>
-					<Input placeholder="请输入文章标题" />
+					<Input
+						value={screeningTitle}
+						onChange={(e) => setScreeningTitle(e.target.value)}
+						placeholder="请输入文章标题"
+					/>
 				</div>
 				<div>
-					<Button type="primary">查询</Button>
-					<Button danger>重置</Button>
+					<Button onClick={screeningPass} type="primary">
+						查询
+					</Button>
+					<Button onClick={screeningReturn} danger>
+						重置
+					</Button>
 				</div>
 			</Screening>
 			<div className="main-block">
 				<TitleBlock title="文章管理">
-					<Button onClick={showDrawer} type="primary">
+					<Button onClick={() => setVisible(true)} type="primary">
 						添加文章
 					</Button>
 					<Button danger>删除文章</Button>
@@ -210,10 +235,10 @@ const Index = function () {
 							align="center"
 							key="id"
 							render={(state) => (
-								<>
-									<i />
+								<div className="state-table">
+									<i className="state-icon state-err-icon" />
 									<span>{state ? "显示" : "隐藏"}</span>
-								</>
+								</div>
 							)}
 						/>
 						<Table.Column title="浏览量" dataIndex="views" width={150} align="center" key="id" />
@@ -225,6 +250,7 @@ const Index = function () {
 							key="id"
 							render={() => (
 								<>
+									{/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/interactive-supports-focus */}
 									<span onClick={aaaaa} className="edit-btn" role="button">
 										编辑
 									</span>
@@ -255,69 +281,97 @@ const Index = function () {
 				title="添加文章"
 				visible={visible}
 				closable={false}
-				onClose={onClose}
 				getContainer={false}
+				onClose={drawerFormReturn}
 				width="40%"
 				extra={
 					<Space>
-						<Button onClick={onClickForm} type="primary">
+						<Button onClick={drawerFormPass} type="primary">
 							确定
 						</Button>
-						<Button>取消</Button>
+						<Button onClick={drawerFormReturn}>取消</Button>
 					</Space>
 				}
 			>
-				<Form layout="vertical" form={form}>
-					<div className="form-row">
-						<div className="form-col">
-							<Form.Item name="title" label="文章标题">
+				<Form layout="vertical" form={formRef}>
+					<Row justify="space-between" gutter={16}>
+						<Col span={12}>
+							<Form.Item
+								name="title"
+								label="文章标题"
+								rules={[{required: true, message: "请输入文章标题"}]}
+							>
 								<Input placeholder="请输入文章标题" />
 							</Form.Item>
-						</div>
-						<div className="form-col">
-							<Form.Item name="classify" label="文章分类">
+						</Col>
+						<Col span={12}>
+							<Form.Item
+								name="classify"
+								label="文章分类"
+								rules={[{required: true, message: "请选择文章分类"}]}
+							>
 								<Select placeholder="请选择文章分类">
 									<Select.Option value="xiao">Xiaoxiao Fu</Select.Option>
 									<Select.Option value="mao">Maomao Zhou</Select.Option>
 								</Select>
 							</Form.Item>
-						</div>
-					</div>
-					<div className="form-row">
-						<div className="form-col">
-							<Form.Item name="source" initialValue="本站" label="文章来源">
+						</Col>
+					</Row>
+					<Row justify="space-between" gutter={16}>
+						<Col span={12}>
+							<Form.Item
+								name="source"
+								initialValue="本站"
+								label="文章来源"
+								rules={[{required: true, message: "请输入文章来源"}]}
+							>
 								<Input placeholder="请输入文章来源" />
 							</Form.Item>
-						</div>
-						<div className="form-col">
-							<Form.Item name="sourceUrl" label="来源链接">
-								<Input addonBefore="http://" placeholder="请输入来源链接" />
+						</Col>
+						<Col span={12}>
+							<Form.Item
+								name="sourceUrl"
+								initialValue="maxiaomao.com"
+								label="来源链接"
+								rules={[{required: true, message: "请输入来源链接"}]}
+							>
+								<Input addonBefore="https://" placeholder="请输入来源链接" />
 							</Form.Item>
-						</div>
-					</div>
-					<div className="form-row">
-						<div className="form-col">
-							<Form.Item name="state" label="文章状态">
+						</Col>
+					</Row>
+					<Row justify="space-between" gutter={16}>
+						<Col span={12}>
+							<Form.Item
+								name="state"
+								initialValue="展示"
+								label="文章状态"
+								rules={[{required: true, message: "请选择文章状态"}]}
+							>
 								<Select placeholder="请选择文章状态">
 									<Select.Option value="xiao">展示</Select.Option>
 									<Select.Option value="mao">隐藏</Select.Option>
 								</Select>
 							</Form.Item>
-						</div>
-						<div className="form-col">
-							<Form.Item name="time" label="发布时间">
-								<DatePicker showTime style={{width: "100%"}} />
+						</Col>
+						<Col span={12}>
+							<Form.Item
+								name="time"
+								initialValue={moment()}
+								label="发布时间"
+								rules={[{required: true, message: "请选择发布时间"}]}
+							>
+								<DatePicker showTime style={{width: "100%"}} placeholder="请选择发布时间" />
 							</Form.Item>
-						</div>
-					</div>
-					<Form.Item name="body" label="文章内容">
-						<MdEditor
-							style={{height: "300px"}}
-							renderHTML={(text) => mdParser.render(text)}
-							onChange={handleEditorChange}
-						/>
-					</Form.Item>
+						</Col>
+					</Row>
 				</Form>
+				<MdEditor
+					value={formBody}
+					renderHTML={(text) => mdParser.render(text)}
+					onChange={({text}) => setFormBody(text)}
+					style={{height: "400px"}}
+					ref={mdEditorRef}
+				/>
 			</Drawer>
 		</div>
 	)
